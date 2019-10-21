6912095c829dc327ba81d42560c9b484aeb8e0a3
5h4d0wb0y <5h4d0wb0y@protonmail.com>
2019-10-21 13:45:16 +0200

    Bump to version 1.0.1

--- socialbrute/__init__.py
+++ socialbrute/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """5h4d0wb0y"""
 __email__ = '5h4d0wb0y@protonmail.com'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __description__ = 'SocialBrute attempts to crack social networks using a brute force dictionary attack.'
 __copyright__ = "2019, 5h4dowboy"
